5041ef8409aa40da48d611eb5fb82c1f357a0fd2
Pau <pau.pulidocompanys@gmail.com>
2022-06-17 19:47:04 +0200

    downloads selector de muncipi

--- client/src/pages/Downloads/index.js
+++ client/src/pages/Downloads/index.js
@@ -1,18 +1,30 @@
 import { requestNotesSimples } from "../../services/api";
+import React, { useState, useEffect } from "react";
 
 function Downloads() {
-  const obtainNotesSimples = async () => {
-    const resposta = await requestNotesSimples();
-    const tableData = ["municipi,barri,titular,dni titular,data"]
+  const [notesSimples, updateNotes] = useState([]);
+  const [municipiState, updateMunicipi] = useState();
+  useEffect(() => {
+    //fem servir useEffect perquè volem carregar les dades des del principi, quan es carrega el DOM
+    requestNotesSimples().then((res) => {
+      updateNotes(res.data);
+    });
+  }, []);
+
+  function stringifyNotes() {
+    return ["municipi,barri,titular,dni titular,data"]
       .concat(
-        resposta.data.map((item) => {
+        notesSimples.map((item) => {
           const { data, municipi, titular, titular_id, barri } =
             item.attributes;
           return [municipi, barri, titular, titular_id, data].join(",");
         })
       )
       .join("\n");
+  }
 
+  function downloadNotes() {
+    const tableData = stringifyNotes();
     const blob = new Blob([tableData], { type: "text/csv" });
     const url = URL.createObjectURL(blob);
     const anchor = document.createElement("a");
@@ -21,7 +33,19 @@ function Downloads() {
     document.body.appendChild(anchor);
     anchor.click();
     document.body.removeChild(anchor);
-  };
+  }
+
+  // const notesSimples = async () => {
+  //   const resp = await requestNotesSimples();
+  //   console.log(resp);
+  //   return (
+  //     <div className="notes-feed">
+  //       {resp.data.map((item, index) => {
+  //         return <li key={index}>{item.id}</li>;
+  //       })}
+  //     </div>
+  //   );
+  // };
 
   return (
     <main className="downloads">
@@ -29,7 +53,43 @@ function Downloads() {
         Pots decsarregar les dades de les notes simples en format .csv fent
         click al botó
       </p>
-      <button onClick={obtainNotesSimples}>Descarrega les dades</button>
+      <button onClick={downloadNotes}>Descarrega les dades</button>
+      <form id="data-filters">
+        <label for="municipi">Selecciona un municipi:</label>
+        <select
+          name="municipi"
+          id="selector-municipi"
+          value={municipiState}
+          onChange={(e) => {
+            updateMunicipi(e.currentTarget.value);
+          }}
+        >
+          {console.log(municipiState)}
+          <option value="empty"></option>
+          {notesSimples.map((item, key) => {
+            return (
+              <option value={item.attributes.municipi}>
+                {item.attributes.municipi}
+              </option>
+            );
+          })}
+          ;
+        </select>
+      </form>
+      <table className="display-notes">
+        <tbody>
+          {/* {Utilitzar el metode filter per arrays} */}
+          {notesSimples.map((item) => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.attributes.titular}</td>
+              </tr>
+            );
+          })}
+          ;
+        </tbody>
+      </table>
     </main>
   );
 }
